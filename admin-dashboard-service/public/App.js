b5928ee9ff011a9cbc33b07bbc5d326b52757c1f
Nipun-BMF <nipunp@botmedfusion.com>
2024-10-29 17:51:20 +0530

    refactor frontend arch

--- admin-dashboard-service/public/App.js
+++ admin-dashboard-service/public/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-        Welcome to the Admin Dashboard Service
+        Welcome to the User & Booking Dashboard Service
         </p>
         <a
           className="App-link"
