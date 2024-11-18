4a4a28d1e5dda7584a08b2a36b8a8199b54aee4d
Lasitha-BMF <lasithah@botmedfusion.com>
2024-11-18 20:27:42 +0530

    Implement Partner Dashboard and Analytics components; add API integration management features and update dependencies

--- manager-control-service/src/App.js
+++ manager-control-service/src/App.js
@@ -79,14 +79,18 @@ import { ToastContainer } from "react-toastify";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import { SidebarItem } from "./components/sideNavBar";
 import "react-toastify/dist/ReactToastify.css";
+
 import Sidebar from "./components/sideNavBar";
 import AddExperience from "./components/experience/addExperience";
 import ViewExperience from "./components/experience";
-import PartnerNonAPIDetailsFormCreate from "./components/partner/partnerDetailsNonAPICreate";
+import PartnerRegistrationNonAPI from "./components/partner/partnerDetailsNonAPICreate";
 import Login from "./components/partner/login/Login";
-import PartnerAPIDetailsFormCreate from './components/partner/partnerDetailsAPICreate';
+import PartnerAPIDetailsFormCreate from "./components/partner/partnerDetailsAPICreate";
+import PartnerAnalyticsInsights from "./components/partner/analytics";
+import IntegrationManagement from "./components/partner/integrationManagement";
 
-import { LayoutDashboard, Volleyball } from "lucide-react";
+import PartnerDashboard from "./components/partner/dashboard";
+import { LayoutDashboard, ChartColumn, Webhook } from "lucide-react";
 import SupportQuery from "./components/partner/supportquery";
 import FaqPage from "./components/partner/faqpage";
 
@@ -95,26 +99,47 @@ function App() {
     <BrowserRouter>
       <div className="App flex h-screen">
         <Sidebar>
-          <Link to="/dashboard">
+          <Link to="/partner/dashboard">
             <SidebarItem
               icon={<LayoutDashboard size={20} />}
               text="Dashboard"
             />
           </Link>
-          <Link to="/experience">
-            <SidebarItem icon={<Volleyball size={20} />} text="Experience" />
+          <Link to="/partner/analytics">
+            <SidebarItem
+              icon={<ChartColumn size={20} />}
+              text="Analytics & Insights"
+            />
+          </Link>
+          <Link to="/partner/integration">
+            <SidebarItem icon={<Webhook size={20} />} text="Integration API" />
           </Link>
         </Sidebar>
 
         <div className="flex-1 overflow-auto p-6 bg-gray-100">
           <Routes>
             <Route path="/login" element={<Login />} />
-            <Route path="/dashboard" element={<div>Welcome to Dashboard</div>} />
             <Route path="/experience/add" element={<AddExperience />} />
-            <Route path="/dashboard" element={<div></div>} /> {/* Example route */}
             <Route path="/experience" element={<ViewExperience />} />
-            <Route path="/partner/details_registration" element={<PartnerRegistrationNonAPI />} />
-            <Route path="/partner/existing_system_registration" element={<PartnerAPIDetailsFormCreate />} />
+            <Route
+              path="/partner/details_registration"
+              element={<PartnerRegistrationNonAPI />}
+            />
+            <Route
+              path="/partner/existing_system_registration"
+              element={<PartnerAPIDetailsFormCreate />}
+            />
+            <Route path="/partner/support_query" element={<SupportQuery />} />
+            <Route path="/partner/faq" element={<FaqPage />} />
+            <Route path="/partner/dashboard" element={<PartnerDashboard />} />
+            <Route
+              path="/partner/analytics"
+              element={<PartnerAnalyticsInsights />}
+            />
+            <Route
+              path="/partner/integration"
+              element={<IntegrationManagement />}
+            />
           </Routes>
         </div>
 
@@ -125,4 +150,3 @@ function App() {
 }
 
 export default App;
-
--- manager-control-service/src/components/sideNavBar/index.jsx
+++ manager-control-service/src/components/sideNavBar/index.jsx
@@ -1,76 +1,85 @@
 import { MoreVertical, ChevronLast, ChevronFirst } from "lucide-react";
 import { useContext, createContext, useState } from "react";
+import partnerLogo from "../../assets/logos/partnerLogo.png";
 
 const SidebarContext = createContext();
 
 export default function Sidebar({ children }) {
-    const [expanded, setExpanded] = useState(true);
+  const [expanded, setExpanded] = useState(true);
 
-    return (
-        <aside className={`h-screen transition-all ${expanded ? "w-60" : "w-16"}`}>
-            <nav className="h-full flex flex-col bg-white border-r shadow-sm">
-                <div className="p-4 pb-2 flex justify-between items-center">
-                    <img
-                        src="https://img.logoipsum.com/243.svg"
-                        className={`overflow-hidden transition-all ${expanded ? "w-32" : "w-0"}`}
-                        alt=""
-                    />
-                    <button
-                        onClick={() => setExpanded((curr) => !curr)}
-                        className="p-1.5 rounded-lg bg-gray-50 hover:bg-gray-100"
-                    >
-                        {expanded ? <ChevronFirst /> : <ChevronLast />}
-                    </button>
-                </div>
+  return (
+    <aside className={`h-screen transition-all ${expanded ? "w-60" : "w-17"}`}>
+      <nav className="h-full flex flex-col bg-white border-r shadow-sm">
+        <div className="p-4 pb-2 flex justify-between items-center">
+          <div className="ms-3">
+            <img
+              src={partnerLogo}
+              className={`overflow-hidden transition-all ${
+                expanded ? "w-32" : "w-0"
+              }`}
+              alt=""
+            />
+          </div>
+          <button
+            onClick={() => setExpanded((curr) => !curr)}
+            className="p-1.5 rounded-lg bg-gray-50 hover:bg-gray-100"
+          >
+            {expanded ? <ChevronFirst /> : <ChevronLast />}
+          </button>
+        </div>
 
-                <SidebarContext.Provider value={{ expanded }}>
-                    <ul className="flex-1 px-3">{children}</ul>
-                </SidebarContext.Provider>
-
-            </nav>
-        </aside>
-    );
+        <SidebarContext.Provider value={{ expanded }}>
+          <ul className="flex-1 px-3">{children}</ul>
+        </SidebarContext.Provider>
+      </nav>
+    </aside>
+  );
 }
 
 export function SidebarItem({ icon, text, active, alert }) {
-    const { expanded } = useContext(SidebarContext);
+  const { expanded } = useContext(SidebarContext);
 
-    return (
-        <li
-            className={`
-        relative flex items-center py-2 px-3 my-1
+  return (
+    <li
+      className={`
+        relative flex items-center py-2 px-4 my-1
         font-medium rounded-md cursor-pointer
         transition-colors group
-        ${active
-                    ? "bg-gradient-to-tr from-indigo-200 to-indigo-100 text-indigo-800"
-                    : "hover:bg-indigo-50 text-gray-600"
-                }
+        ${
+          active
+            ? "bg-gradient-to-tr from-indigo-200 to-indigo-100 text-indigo-800"
+            : "hover:bg-indigo-50 text-gray-600"
+        }
     `}
-        >
-            {icon}
-            <span
-                className={`overflow-hidden transition-all ${expanded ? "w-52 ml-3" : "w-0"}`}
-            >
-                {text}
-            </span>
-            {alert && (
-                <div
-                    className={`absolute right-2 w-2 h-2 rounded bg-indigo-400 ${expanded ? "" : "top-2"}`}
-                />
-            )}
+    >
+      {icon}
+      <span
+        className={`overflow-hidden transition-all ${
+          expanded ? "w-52 ml-3" : "w-0"
+        }`}
+      >
+        {text}
+      </span>
+      {alert && (
+        <div
+          className={`absolute right-2 w-2 h-2 rounded bg-indigo-400 ${
+            expanded ? "" : "top-2"
+          }`}
+        />
+      )}
 
-            {!expanded && (
-                <div
-                    className={`
+      {!expanded && (
+        <div
+          className={`
           absolute left-full rounded-md px-2 py-1 ml-6
           bg-indigo-100 text-indigo-800 text-sm
           invisible opacity-20 -translate-x-3 transition-all
           group-hover:visible group-hover:opacity-100 group-hover:translate-x-0
       `}
-                >
-                    {text}
-                </div>
-            )}
-        </li>
-    );
+        >
+          {text}
+        </div>
+      )}
+    </li>
+  );
 }
