363332c4c40be02d33e94c3412bdd1ab1dbfce29
seleena <seleenaw@botmedfusion.com>
2024-11-19 14:35:07 +0530

    search-bar

--- user-booking-service/src/App.js
+++ user-booking-service/src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
+// App.js
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+import LandingSearchBar from './landingPage/searchBar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-        Welcome to the User & Booking  Service
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Routes>
+      
+          <Route path="/" element={<LandingSearchBar />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
